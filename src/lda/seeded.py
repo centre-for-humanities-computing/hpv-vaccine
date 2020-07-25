c564eac27546fff5cb00444b2417090d29e903d8
supplyandcommand <42962106+supplyandcommand@users.noreply.github.com>
2020-07-25 14:02:10 +0200

    FIX seeded: var names, paths, todo
    FIX comment of filenames

--- src/lda/seeded.py
+++ src/lda/seeded.py
@@ -1,23 +1,31 @@
-'''
+'''Iterate guidedlda's LDA in search of good hyperparameters.
+
 TODO
-- everything
+- TfIdfVectorizer
+- iterate priors
+- iterate seed_confidence
+- docstring for grid search function
+
+- avg topic coherence
+- per-topic coherence
 '''
 import os
 from itertools import chain
 from time import time
 
-from six.moves import cPickle as pickle
+import pickle
+import ndjson
 
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfIdfVectorizer
 import guidedlda
 import pyLDAvis.sklearn
 
 from src.utility.general import make_folders
 
 
-def get_seeds(texts, seed_topic_list, vectorizer):
+def init_guidedlda(texts, seed_topic_list, vectorizer_type='count'):
     '''
-    Prepare data & seed priors for guidedlda.
+    Prepare data & transform seeds to priors for guidedlda.
     (Vectorize texts and extract hashed seeds)
 
     No more preprocessing is done here.
@@ -32,7 +40,7 @@ def get_seeds(texts, seed_topic_list, vectorizer):
         list of words, where in seed_topic_list[x][y] 
         x is a topic and y a word belonging in that topic.
 
-    vectorizer : str
+    vectorizer_type : str (optional)
         Map documents using raw counts, or tfidf?
         Options = {"count", "tfidf"}
     '''
@@ -41,7 +49,7 @@ def get_seeds(texts, seed_topic_list, vectorizer):
         return doc
 
     # choose vectorizer
-    if vectorizer is 'tfidf':
+    if vectorizer_type is 'tfidf':
         # TODO: this is dummy code
         vectorizer = TfIdfVectorizer(
             analyzer='word',
@@ -50,7 +58,7 @@ def get_seeds(texts, seed_topic_list, vectorizer):
             lowercase=False
         )
 
-    elif vectorizer is 'count':
+    elif vectorizer_type is 'count':
         vectorizer = CountVectorizer(
             analyzer='word',
             tokenizer=do_nothing,
@@ -59,7 +67,7 @@ def get_seeds(texts, seed_topic_list, vectorizer):
         )
 
     else:
-        raise ValueError("vectorizer: choose tfidf, or count")
+        raise ValueError('vectorizer_type: choose "tfidf", or "count"')
 
     # prep texts to guidedlda format
     X = vectorizer.fit_transform(texts)
@@ -78,13 +86,16 @@ def get_seeds(texts, seed_topic_list, vectorizer):
     return X, seed_priors, vectorizer
 
 
-def iterate_guidedlda(X, seed_priors, vect_obj,
+def iterate_guidedlda(X, seed_priors, vectorizer,
                       n_topics_range, priors_range,
-                      out_dir,
+                      out_dir, iterations=2000,
                       verbose=True):
     '''
     Fit many topic models to pick the most tuned hyperparameters.
     Guidedlda version.
+
+    Each fitted model is saved, filename being in the following format:
+    {number of topics}T_{iteration rank}I_.{file extension}
 
 
     Parameters
@@ -95,24 +106,38 @@ def iterate_guidedlda(X, seed_priors, vect_obj,
     seed_priors : ???
         array of hashed seeds to be feeded as priors for the model.
 
-    vect_obj : ???
+    vectorizer : ???
         vectorizer pipeline object
         Used for pyLDAvisualization.
+
+    n_topics_range : ???
+        XX
+
+    priors_range : tuple
+        where 
+        priors_range[0] is range for alpha 
+        priors_range[1] is range for eta.
 
     out_dir : str
         path to a directory, where results will be saved (in a child directory).
 
+    iterations : int (default: 2000)
+        maximum number of iterations to fit a topic model with.
+
+    verbose : bool (default: True)
+        print progress comments?
+
 
     Exports
     -------
-    report_lines/*
+    out_dir/report_lines/*
         pickled dict with model information
         (n topics, model coherence, per-topic coherence, hyperparameters)
         
-    models/*
+    out_dir/models/*
         gensim objects, where the model is saved.
         
-    plots/*
+    out_dir/plots/*
         pyLDAvis visualizations of the model
     '''
     # prepare foldrs
@@ -136,13 +161,13 @@ def iterate_guidedlda(X, seed_priors, vect_obj,
         # iterate over priors
         # TODO
         smallio = []
-        for alpha, eta in chain(priors_range):
+        for alpha, eta in priors_range:
 
             start_time = time() # track time
             i += 1 # track iterations
 
             # paths for saving
-            filename = str(n_top) + "T_" + str(i)
+            filename = str(n_top) + "T_" + str(i) + "I_"
             report_path = os.path.join(report_folder + filename + '.pickle')
             model_path = os.path.join(model_folder + filename + '.model')
             pyldavis_path = os.path.join(plot_folder + filename + '_pyldavis.html')
@@ -150,7 +175,7 @@ def iterate_guidedlda(X, seed_priors, vect_obj,
             # train model
             model = guidedlda.GuidedLDA(
                 n_topics=n_top,
-                n_iter=2000,
+                n_iter=iterations,
                 alpha=alpha, eta=eta,
                 random_state=7, refresh=10
             )
@@ -171,28 +196,30 @@ def iterate_guidedlda(X, seed_priors, vect_obj,
     #             print('    Coherence: {}'.format(coh_score.round(2)))
 
             # save report
-            # TODO: fuck pickle, let's ndjson
             report = (n_top, alpha, eta, training_time, coh_score, coh_topics)
-            report_list.append(report)
-            with open(report_path, 'wb') as f:
-                pickle.dump(report, f)
+            with open(report_path, 'w') as f:
+                ndjson.dump(report, f)
 
             # save model
-            # TODO: sklearn
-            model.save(model_path)
+            with open(model_path, 'wb') as f:
+                pickle.dump(model, f)
 
             # produce a visualization
-            nice = pyLDAvis.sklearn.prepare(model, X, vect_obj)
+            nice = pyLDAvis.sklearn.prepare(model, X, vectorizer)
             pyLDAvis.save_html(nice, pyldavis_path)
 
     return None
 
 
 def grid_search_lda_SED(texts, seed_topic_list, vectorizer):
+    '''
+    Wrapper for functions init_guidedlda & iterate_guidedlda.
+    For documentation, see their docstrings.
+    '''
     
-    X, seed_priors, vect_obj = get_seeds(texts, seed_topic_list, vectorizer)
+    X, seed_priors, vectorizer = get_seeds(texts, seed_topic_list, vectorizer)
     
-    iterate_guidedlda(X, seed_priors, vect_obj,
+    iterate_guidedlda(X, seed_priors, vectorizer,
                       n_topics_range, priors_range
                       report_folder, model_folder, plot_folder,
                       verbose=True)
